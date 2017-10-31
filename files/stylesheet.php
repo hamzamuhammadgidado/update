beaf785e5cfd434017c5ffa5f2351cc432e09bec
Gidado <ibrahimgidado@gmail.com>
2017-10-31 21:44:25 +0100

    Add Folder For Working

---
+++ files/stylesheet.php
@@ -0,0 +1,12 @@
+<!--- LInk Style Sheet ---->
+  <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="../css/index.css">
+  <link rel="stylesheet" type="text/css" href="../css/main.css">
+  <link rel="stylesheet" type="text/css" href="../css/icons.css">
+  <link rel="stylesheet" type="text/css" href="../css/animate.css">
+  <link rel="stylesheet" type="text/css" href="../css/media.css">
+<!--- End Link Style Sheet ----->
+
+<!--- LInk Icon Header ---->
+  <link rel="icon" type="text/css" href="../image/icon-title.png">
+<!--- End Link Icon Header ----->
